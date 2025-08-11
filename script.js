85de0c703861c35def6d6ea8e649cda1a97e1990
Michael Thede <michael.r.thede@gmail.com>
2025-08-11 09:36:57 -0400

    feat: use javascript to make more UI for RPS

--- script.js
+++ script.js
@@ -1,53 +1,54 @@
-console.log("Hello World!");
+let playerScore = 0;
+let computerScore = 0;
+let computerChoice = 0;
+let num = 0
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorButton = document.querySelector("#scissors");
+const scoreboard = document.querySelector("#scoreboard");
+const scoreText = document.querySelector("#scoreText");
 
-function compChoice() {
+function computerPicker(){
     num = Math.random();
+    console.log(num);
     if(num <= 0.33){
-        return "Rock";
+        return "paper";
     } else if (num <= 0.66 && num > 0.33){
-        return "Paper";
+        return "rock";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-function playChoice() {
-    ans = prompt("Choose Rock, Paper, or Scissors!");
-    return ans;
+function playRound(playerChoice, computerChoice){
+    if(playerChoice == "rock" && computerChoice == "rock" ||
+        playerChoice == "paper" && computerChoice == "paper" ||
+        playerChoice == "scissors" && computerChoice == "scissors"){
+        scoreText.textContent = "Tie!"
+    } else if (playerChoice == "rock" && computerChoice == "scissors" ||
+        playerChoice == "paper" && computerChoice == "rock" ||
+        playerChoice == "scissors" && computerChoice == "paper"){
+        scoreText.textContent = "Player Wins! " + playerChoice + " beats " + computerChoice;
+        playerScore++;
+    } else {
+        scoreText.textContent = "Computer Wins!" + computerChoice + " beats " + playerChoice;
+        computerScore++;
+    }
+    scoreboard.textContent = "Player: " + playerScore + "Computer: " + computerScore
+    if(computerScore == 5){
+        scoreboard.textContent = "Computer Wins Round!"
+        scoreText.textContent = "Starting New Round!"
+        computerScore = 0;
+        playerScore = 0;
+    } else if (playerScore == 5){
+        scoreboard.textContent = "Player Wins Round!"
+        scoreText.textContent = "Starting New Round!"
+        computerScore = 0;
+        playerScore = 0;
+    }
 }
 
-function playGame(){
+rockButton.addEventListener("click", () => playRound("rock", computerPicker()));
+paperButton.addEventListener("click", () => playRound("paper", computerPicker()));
+scissorButton.addEventListener("click", () => playRound("scissors", computerPicker()));
 
-    function playRound(playPick, compPick) {
-    playPick = playPick.toLowerCase();
-    if((playPick === "rock" && compPick === "Rock") || (playPick === "paper" && compPick === "Paper") || (playPick === "scissors" && compPick === "Scissors")){
-        console.log("It's a draw! Play again!")
-    } else if ((playPick === "rock" && compPick === "Scissors") || (playPick === "paper" && compPick === "Rock") || (playPick === "scissors" && compPick === "Paper")){
-        console.log("You win! " + playPick + " beats " + compPick + "!");
-        playScore++;
-    } else {
-        console.log("You lose! " + compPick + " beats " + playPick + "!");
-        compScore++;
-    }
-    }
-
-    playScore = 0
-    compScore = 0
-
-    while(playScore < 3 && compScore < 3){
-        playPick = playChoice();
-        compPick = compChoice();
-        playRound(playPick, compPick);
-        console.log("Current score is Player: " + playScore + " and Computer: " + compScore);
-    }
-
-    if(playScore === 3){
-        console.log("Player Wins!")
-    } else {
-        console.log("Computer Wins!")
-    }
-
-
-}
-
-playGame();
